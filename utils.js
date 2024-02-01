2d9a00e8d0ec87f81ef8510ff9985f89e20b227e
#Dm <108529559+DmitryDemidov48@users.noreply.github.com>
2024-02-01 16:35:28 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,25 +1,33 @@
+// Функция для определения победителя на основе здоровья игроков и таймера.
 function determineWinner({player, enemy, timerId}) {
-    clearTimeout(timerId)
-    document.querySelector("#displayText").style.display = "flex"
+    clearTimeout(timerId); // Очистка таймера
+
+    // Отображение текста о победителе
+    document.querySelector("#displayText").style.display = "flex";
+
+    // Определение победителя на основе здоровья игроков
     if (player.health === enemy.health) {
-        document.querySelector("#displayText").innerHTML = 'Tie'
+        document.querySelector("#displayText").innerHTML = 'Tie'; // Ничья
     } else if (player.health > enemy.health) {
-        document.querySelector("#displayText").innerHTML = 'Player 1 Wins'
+        document.querySelector("#displayText").innerHTML = 'Player 1 Wins'; // Победа игрока 1
     } else if (player.health < enemy.health) {
-        document.querySelector("#displayText").innerHTML = 'Player 2 Wins'
-    }
-}
-//timer
-let timer = 60
-let timerId
-function decreaseTimer() {
-    if (timer > 0) {
-        timerId =  setTimeout(decreaseTimer, 1000)
-        timer--
-        document.querySelector("#timer").innerHTML = timer
-    }
-    if (timer === 0) {
-        determineWinner({player, enemy,timerId})
+        document.querySelector("#displayText").innerHTML = 'Player 2 Wins'; // Победа игрока 2
     }
 }
 
+// Функция для уменьшения таймера на 1 каждую секунду.
+let timer = 60; // Изначальное значение таймера
+let timerId; // Идентификатор таймера
+
+function decreaseTimer() {
+    if (timer > 0) {
+        timerId = setTimeout(decreaseTimer, 1000); // Запуск функции снова через 1 секунду
+        timer--; // Уменьшение значения таймера
+        document.querySelector("#timer").innerHTML = timer; // Отображение текущего значения таймера
+    }
+
+    // Если таймер достигает 0, вызываем функцию determineWinner
+    if (timer === 0) {
+        determineWinner({player, enemy, timerId});
+    }
+}
